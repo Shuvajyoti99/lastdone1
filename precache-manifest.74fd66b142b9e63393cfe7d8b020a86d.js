3d07f6ef13166a306abeb8c35cfb5306b366ad2c
Shuvajyoti99 <63437730+Shuvajyoti99@users.noreply.github.com>
2020-04-19 10:17:15 +0530

    Add files via upload

---
+++ precache-manifest.74fd66b142b9e63393cfe7d8b020a86d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dbff4a68fc7f798220d931f57239a06",
+    "url": "/lastdone1/index.html"
+  },
+  {
+    "revision": "0800a8f736d46cf17e73",
+    "url": "/lastdone1/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "1be62cbb6bf94ede5411",
+    "url": "/lastdone1/static/css/main.0d16e165.chunk.css"
+  },
+  {
+    "revision": "0800a8f736d46cf17e73",
+    "url": "/lastdone1/static/js/2.df3de0a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lastdone1/static/js/2.df3de0a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be62cbb6bf94ede5411",
+    "url": "/lastdone1/static/js/main.44a1492f.chunk.js"
+  },
+  {
+    "revision": "cca1deb53bb2b178a48a",
+    "url": "/lastdone1/static/js/runtime-main.7c149286.js"
+  }
+]);
